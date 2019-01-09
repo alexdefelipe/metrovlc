7790a85c09aba7a508d0ce83db4fed93339380fd
Alejandro De Felipe Machancoses <alexdefelipe@gmail.com>
2019-01-09 19:37:13 +0100

    Añadida validación reactiva del formulario de contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -11,6 +11,25 @@ export class ContactoComponent implements OnInit {
   consultaForm: FormGroup;
   consulta: Consulta;
 
+  erroresForm = {
+    'nombre': '',
+    'email': ''
+    'mensaje': '',
+  };
+
+  mensajesError = {
+    'nombre': {
+      'required': 'El nombre es obligatorio.',
+    },
+    'email': {
+      'required': 'La dirección de email es obligatoria.',
+      'email': 'La dirección de email no tiene el formato correcto.'
+    },
+    'apellidos': {
+      'required': 'Los apellidos son obligatorios.',
+    }
+  };
+
   constructor(private fb: FormBuilder) {
     this.crearFormulario();
   }
@@ -20,16 +39,34 @@ export class ContactoComponent implements OnInit {
 
   crearFormulario() {
     this.consultaForm = this.fb.group({
-      nombre: "",
-      correo: "",
+      nombre: ["", Validators.required],
+      correo: ["", [Validators.required, Validators.email]],
       mensaje: ""
     });
+    this.consultaForm.valueChanges.subscribe(datos => this.onCambioValor(datos));
+    this.onCambioValor();
   }
 
   onSubmit() {
     this.consulta = this.consultaForm.value;
     console.log(this.consulta);
     this.consultaForm.reset();
+  }
+
+  onCambioValor(data?: any) {
+    if (!this.consultaForm) { return; }
+    const form = this.consultaForm;
+    for (const field in this.erroresForm) {
+      // Se borrarán los mensajes de error previos
+      this.erroresForm[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.mensajesError[field];
+        for (const key in control.errors) {
+          this.erroresForm[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
 }
