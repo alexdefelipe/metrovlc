a3fa0c51ffadf77ab4ef933375528adb58eea33a
Alejandro De Felipe Machancoses <alexdefelipe@gmail.com>
2019-01-09 19:51:39 +0100

    Tratado de errores HTTP

--- src/app/services/horarios.service.ts
+++ src/app/services/horarios.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ProcesaHTTPMsjService } from '../services/procesa-httpmsj.service';
+
 // Para utilizar HttpClient
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -9,9 +11,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HorariosService {
 
-  constructor(private http: HttpClient) { } // Se pasa un objecto de la clase HttpClient al constructor
+  constructor(private http: HttpClient, private procesaHttpMsjService: ProcesaHTTPMsjService) { } // Se pasa un objecto de la clase HttpClient al constructor
 
   getHorariosParaTrayecto(id_est_origen:string, id_est_dest:string, fecha:string): Observable<string> {
-    return this.http.get<string>("https://metrovlcschedule.herokuapp.com/api/v1/routes?from="+id_est_origen+"&to="+id_est_dest+"&date="+fecha);
+    return this.http.get<string>("https://metrovlcschedule.herokuapp.com/api/v1/routes?from="+id_est_origen+"&to="+id_est_dest+"&date="+fecha)
+    .pipe(catchError(this.procesaHttpMsjService.gestionError));;
   }
 }
