f61db0526d33f310c70865da16b988208968c285
Alejandro De Felipe Machancoses <alexdefelipe@gmail.com>
2019-01-12 17:08:57 +0100

    Configurada prueba unitaria para el componente de registro

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -2,15 +2,63 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 
+import { Usuario } from '../compartido/usuario';
+import { baseURL } from '../compartido/baseurl';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UsuarioService } from "../services/usuario.service";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
+    let usuarioServiceReplica = {
+      registrarUsuario: function(): void {
+
+      }
+    }
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule, FormsModule,
+        MatDialogModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatTableModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatSlideToggleModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        RouterTestingModule.withRoutes([{ path: 'register', component: RegisterComponent }])],
+      providers: [{ provide: UsuarioService, useValue: usuarioServiceReplica },
+      { provide: "BaseURL", useValue: baseURL }]
     })
-    .compileComponents();
+      .compileComponents();
+    let usuarioservice = TestBed.get(UsuarioService);
   }));
 
   beforeEach(() => {
